f9c425a0abc986ab12ff271f4a1d4b1ce374fe05
Hajer Taktak <hajer.taktak@tek-up.de>
2023-02-02 12:12:48 +0100

    Ajoutez des fichiers projet.

---
+++ FrameworkDotNetGLSI-C/Controllers/MovieController.cs
@@ -0,0 +1,45 @@
+﻿using FrameworkDotNetGLSI_C.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrameworkDotNetGLSI_C.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public IEnumerable<Movie> getMovies()
+        {
+            var movies= new List<Movie>(){
+                new Movie{Id=1, Title="Film 1"},
+                new Movie{Id=2, Title="Film 2"}
+            };
+            return movies;
+        }
+        public ActionResult Index()
+        {
+            var m = getMovies();
+            
+            return View(m);
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("Id est  " + id);
+            
+        }
+        //Définir route de chaque contrôleur dans des annotations
+        [Route("Date/{year}/{month}")]
+        public ActionResult DateYearMonth(int year, int month)
+        {
+            return Content(year+"   "+month);
+        }
+        public ActionResult details(int id)
+        {
+            var movieDetails = getMovies().First(c => c.Id == id);
+            return View(movieDetails);
+        }
+      
+    }
+}
---
+++ FrameworkDotNetGLSI-C/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales sur un assembly sont contrôlées via ce
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("FrameworkDotNetGLSI_C")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FrameworkDotNetGLSI_C")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L’affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("01c8eca2-3001-4e24-a5c8-dbb2a565c0ca")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Major Version
+//      Version mineure
+//      Build Number
+//      Revision
+//
+// Vous pouvez spécifier toutes les valeurs ou vous pouvez définir par défaut les valeurs Révision et Numéro de version
+// en utilisant '*', comme indiqué ci-dessous :
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
